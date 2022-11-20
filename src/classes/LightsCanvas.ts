8592da05eb01a0fac8bf1a5782df9600b46c1bad
pritulaziah <ziahherzen@gmail.com>
2022-11-20 10:57:31 +0500

    create debug for lights

--- src/classes/LightsCanvas.ts
+++ src/classes/LightsCanvas.ts
@@ -1,15 +1,35 @@
 import DefaultCanvas from "classes/common/DefaultCanvas";
+import * as THREE from "three";
 
 class LightsCanvas extends DefaultCanvas {
   constructor(canvas: HTMLCanvasElement) {
     super(canvas);
   }
 
+  static lightsCollection = {
+    AmbientLight: THREE.AmbientLight,
+    PointLight: THREE.PointLight,
+  };
+
+  createDebugLights() {
+    for (const [lightName, lightInstance] of Object.entries(
+      LightsCanvas.lightsCollection
+    )) {
+      const targetLight = this.lights.find(
+        (light) => light instanceof lightInstance
+      );
+
+      if (targetLight) {
+        this.gui
+          .addFolder(lightName)
+          .add(targetLight, "intensity", 0, 1, 0.001);
+      }
+    }
+  }
+
   createDebug() {
     if (this.lights.length > 0) {
-      const folderLight = this.gui.addFolder("Light");
-      folderLight.add(this.lights[0], "intensity", 0, 1, 0.001);
-      folderLight.open();
+      this.createDebugLights();
     }
   }
 }
