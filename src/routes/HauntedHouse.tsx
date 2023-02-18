131c575bc33b832cf656b13dc9df529e30d86fcf
pritulaziah <ziahherzen@gmail.com>
2023-02-18 17:52:17 +0500

    add graves and bushes

--- src/routes/HauntedHouse.tsx
+++ src/routes/HauntedHouse.tsx
@@ -4,13 +4,21 @@ import DefaultCanvas from "classes/common/DefaultCanvas";
 import * as THREE from "three";
 import DefaultObject from "classes/common/DefaultObject";
 
+const WORLD_SIZE = 20;
+const HOUSE_HEIGHT = 2.5;
+const HOUSE_WIDTH = 4;
+const ROOF_HEIGHT = 1;
+const DOOR_HEIGHT = 2;
+const GRAVE_WIDTH = 0.6;
+const GRAVE_HEIGHT = 0.8;
+
 const initCanvas = (canvasElement: HTMLCanvasElement) => {
   const canvas = new DefaultCanvas(canvasElement);
   canvas.setCameraPosition({ x: 4, y: 2, z: 5 });
   // Objects
   const floor = new DefaultObject(
     new THREE.Mesh(
-      new THREE.PlaneGeometry(20, 20),
+      new THREE.PlaneGeometry(WORLD_SIZE, WORLD_SIZE),
       new THREE.MeshStandardMaterial({
         color: "#a9c388",
         side: THREE.DoubleSide,
@@ -23,19 +31,69 @@ const initCanvas = (canvasElement: HTMLCanvasElement) => {
       },
     }
   );
-  const houseGroup = new THREE.Group();
+  const house = new THREE.Group();
+  // Walls
   const walls = new THREE.Mesh(
-    new THREE.BoxGeometry(4, 2.5, 4),
+    new THREE.BoxGeometry(HOUSE_WIDTH, HOUSE_HEIGHT, HOUSE_WIDTH),
     new THREE.MeshStandardMaterial({ color: "#ac8382" })
   );
-  walls.position.y = 1.25;
-  houseGroup.add(walls);
+  walls.position.y = HOUSE_HEIGHT / 2;
+  // Roof
+  const roof = new THREE.Mesh(
+    new THREE.ConeGeometry(3.5, ROOF_HEIGHT, 4),
+    new THREE.MeshStandardMaterial({ color: "#b35f45" })
+  );
+  roof.position.y = HOUSE_HEIGHT + ROOF_HEIGHT / 2;
+  roof.rotation.y = Math.PI * 0.25;
+  // Door
+  const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(2, DOOR_HEIGHT),
+    new THREE.MeshStandardMaterial({ color: "#aa7b7b" })
+  );
+  door.position.y = DOOR_HEIGHT / 2;
+  door.position.z = HOUSE_WIDTH / 2 + 0.01;
+  // Bushes
+  const bushes = new THREE.Group();
+  const bushGeometry = new THREE.SphereGeometry(1, 16, 16);
+  const bushMaterial = new THREE.MeshStandardMaterial({ color: "#89c854" });
+  const bush1 = new THREE.Mesh(bushGeometry, bushMaterial);
+  bush1.scale.set(0.5, 0.5, 0.5);
+  bush1.position.set(0.8, 0.2, 2.2);
+  const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
+  bush2.scale.set(0.25, 0.25, 0.25);
+  bush2.position.set(1.4, 0.1, 2.1);
+  const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
+  bush3.scale.set(0.4, 0.4, 0.4);
+  bush3.position.set(-0.8, 0.1, 2.2);
+  const bush4 = new THREE.Mesh(bushGeometry, bushMaterial);
+  bush4.scale.set(0.15, 0.15, 0.15);
+  bush4.position.set(-1, 0.05, 2.6);
+  bushes.add(bush1, bush2, bush3, bush4);
+  // Graves
+  const graves = new THREE.Group();
+  const graveGeometry = new THREE.BoxGeometry(GRAVE_WIDTH, GRAVE_HEIGHT, 0.2);
+  const graveMaterial = new THREE.MeshStandardMaterial({ color: "#b2b6b1" });
 
-  const house = new DefaultObject(houseGroup, {
-    debug(object, gui) {},
-  });
+  for (let i = 0; i < 50; i++) {
+    const angle = Math.random() * Math.PI * 2;
+    const radius =
+      HOUSE_WIDTH +
+      Math.random() * (WORLD_SIZE / 2 - HOUSE_WIDTH - GRAVE_WIDTH / 2);
+    const x = Math.sin(angle) * radius;
+    const z = Math.cos(angle) * radius;
 
-  const objects = [floor, house];
+    const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+    grave.position.set(x, GRAVE_HEIGHT / 2 - 0.1, z);
+    grave.rotation.x = (Math.random() - 0.5) * 0.4;
+    grave.rotation.z = (Math.random() - 0.5) * 0.4;
+    graves.add(grave);
+  }
+
+  // Add all
+  house.add(walls, roof, door, bushes, graves);
+  const houseObject = new DefaultObject(house);
+
+  const objects = [floor, houseObject];
   // Lights
   const ambientLight = new DefaultObject(
     new THREE.AmbientLight(0xffffff, 0.5),
